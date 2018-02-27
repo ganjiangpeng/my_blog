1018551dc02c97c9682134be1a7898a953208b59
Jiangpeng Gan <ganjiangpeng@163.com>
2018-02-27 14:19:26 +0800

    修改

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,12 @@
 from . import main
-
+from flask import render_template
 @main.route('/')
 def index():
     return 'index pager1'
 
 
+@main.route('/user/<name>')
+def users(name):
+    books=['python','php','java','lua','shell']
+    message = {'name':'gan','age':18,'sex':'M'}
+    return render_template('users.html',name=name,books=books,message=message)
