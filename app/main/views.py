29590f69393173a8bae4678d92b970cdda788def
Jiangpeng Gan <ganjiangpeng@163.com>
2018-04-23 18:34:10 +0800

    用户信息显示及修改

--- app/main/views.py
+++ app/main/views.py
@@ -2,10 +2,11 @@
 from . import main
 from flask import render_template,abort,flash,redirect,url_for
 from datetime import datetime
-from ..models import User
-from forms import EditProfileForm
-from flask_login import current_user
+from ..models import User,Role,Permission
+from forms import EditProfileForm,EditProfileAdminForm
+from flask_login import current_user,login_required
 from ..import db
+from ..decorators import permission_required,admin_required
 
 @main.route('/')
 def index():
@@ -19,6 +20,7 @@ def user(username):
         abort(404)
     return render_template('user.html',user=user)
 @main.route('/edit-profile',methods=['POST','GET'])
+@login_required
 def edit_profile():
     form = EditProfileForm()
     if form.validate_on_submit():
@@ -32,3 +34,35 @@ def edit_profile():
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html',form=form)
+
+@main.route('/edit-profile/<int:id>',methods=["POST","GET"])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        return redirect(url_for('.user',username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html',form=form,user=user)
+
+
+@main.route('/test/<int:id>')
+#@permission_required(Permission.ADMINISTER)
+@admin_required
+def test(id):
+    return "123"
